7c43d3b35602682789fa240563ecad1b78c6c664
Joshua Monroe <59244307+joshuamonroe@users.noreply.github.com>
2023-12-31 07:32:07 -0600

    Completed chapter 2

--- code-ch02/ecc.py
+++ code-ch02/ecc.py
@@ -142,7 +142,7 @@ class Point:
 
     def __ne__(self, other):
         # this should be the inverse of the == operator
-        raise NotImplementedError
+        return not (self == other)
 
     def __repr__(self):
         if self.x is None:
@@ -154,7 +154,7 @@ class Point:
     def __add__(self, other):  # <2>
         if self.a != other.a or self.b != other.b:
             raise TypeError('Points {}, {} are not on the same curve'.format
-            (self, other))
+                            (self, other))
 
         if self.x is None:  # <3>
             return other
@@ -163,21 +163,33 @@ class Point:
         # end::source3[]
 
         # Case 1: self.x == other.x, self.y != other.y
+        # Exception: self == other, self.y = 0 -> vertical tangent line
         # Result is point at infinity
+        if (self.x == other.x and self.y != other.y) or \
+            (self == other and self.y == 0):
+            return self.__class__(None, None, self.a, self.b)
 
         # Case 2: self.x ≠ other.x
         # Formula (x3,y3)==(x1,y1)+(x2,y2)
         # s=(y2-y1)/(x2-x1)
         # x3=s**2-x1-x2
         # y3=s*(x1-x3)-y1
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
 
         # Case 3: self == other
         # Formula (x3,y3)=(x1,y1)+(x1,y1)
         # s=(3*x1**2+a)/(2*y1)
         # x3=s**2-2*x1
         # y3=s*(x1-x3)-y1
+        else:
+            s = (3 * self.x**2 + self.a) / 2 * self.y
+            x = s**2 - 2 * self.x
+            y = s * (self.x - x) - self.y
 
-        raise NotImplementedError
+        return self.__class__(x, y, self.a, self.b)
 
 
 class PointTest(TestCase):
